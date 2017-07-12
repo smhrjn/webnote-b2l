804edbae0279aeb3954a8ee304acc551de83d307
leovcunha <lvcunha@gmail.com>
2017-07-12 20:54:25 -0300

    login page

---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="login-component">
+		<card class="card-content">
+			<form action="/login" method="post" class="login-form">
+				Name:<br>
+				<input type="text" name="name" value=""><br><br>
+                Password:<br>
+				<input type="password" name="password" value=""><br><br>
+				<input type="submit" value="Submit">
+			</form>
+		</card>
+	</div>
+</template>
+
+<script>
+	import Card from '../components/Card.vue';
+	export default {
+		name: 'login',
+		components: {Card},
+		data() {
+			return {
+
+			};
+		}
+	};
+</script>
+
+<style lang="scss">
+	@import "~styles/variables.scss";
+
+	.login-component {
+		background-color: $accent-color;
+		text-align: center;
+	}
+
+</style>
