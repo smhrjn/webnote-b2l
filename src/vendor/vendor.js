b5c1a4e73bdd4573dccc0d369970efb7ea6e41f7
saratmaharjan <sarat.maharjan@gmail.com>
2017-07-15 20:56:52 +0200

    npmDeploy

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -3,8 +3,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import axios from 'axios';
+import moment from 'moment';
 
-// import 'normalize.css/normalize.css';
+import 'normalize.css/normalize.css';
 // import 'flexboxgrid/dist/flexboxgrid.css';
 
 import './bootstrap.scss'
