1d7198a535b89c6430ac34e21259474d78e41fee
leovcunha <lvcunha@gmail.com>
2017-07-19 14:46:31 +0000

    web reader completed

--- server/middleware/web-reader.js
+++ server/middleware/web-reader.js
@@ -1,32 +1,36 @@
 const read = require('node-readability')
 const sanitizer = require('sanitizer')
 
-module.exports = (req, res, next) {
-    read(req.body.weburl, function(err, article, meta) {
-        if (err) {
-			return res.json(err);
+//Module that reads a web page and takes its text out using both modules above
+//output is sent to note creating route
+
+
+module.exports = (url, next) => {
+    read(url, function(err, article, meta) {
+        if (err) return next(err, false);
+
         const scrapedArticle = {
-            url: req.body.weburl,
+            url: url,
             title: article.title.trim(),
-            content: stripHTML(article.content || "" );
+            content: stripHTML(article.content || "" )
         }
         article.close();
-        next(scrapedArticle);
+        next(null, scrapedArticle);
     });
 
-});
+}
 
 function stripHTML(html) {
     let clean = sanitizer.sanitize(html, function (str) {
         return str;
     });
-    // Remove all remaining HTML tags.
+    // Remove remaining HTML tags.
     clean = clean.replace(/<(?:.|\n)*?>/gm, "");
 
 
-    // See: http://stackoverflow.com/questions/816085/removing-redundant-line-breaks-with-regular-expressions
+    //http://stackoverflow.com/questions/816085/removing-redundant-line-breaks-with-regular-expressions
     clean = clean.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/ig, "\n");
 
-    // Return the final string, minus any leading/trailing whitespace.
+    // Return the resulting text trimming whitespace.
     return clean.trim();
 }
